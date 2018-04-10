2bf972171d5d41243c56acac4e2e7cb5c449f49d
Jonathan Torres <Jonathan.torres25@live.com>
2018-04-09 21:39:56 -0400

    http to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const ROOT_URL = "https://reduxblog.herokuapp.com/api";
 const API_KEY = '?key=Jona1234';
 
 export const FETCH_POSTS = "fetch_posts";
